cb056e6a30b5d4cd1d5d39106ccda31d82b9ee3e
ErickDC <erickdiazc717@gmail.com>
2023-03-16 14:33:53 -0500

    services component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './components/home/Home'
+import Services from './components/services/Services'
 
 function App() {
   return (
     <main className="main">
       <Home />
+      <Services />
     </main>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,76 +9,78 @@ import './home.css'
 const Home = () => {
     return (
         <section className="home" id="home">
-            <div className="home__container container">
-                <p className="home__subtitle text-cs">
-                    Hello, <span>My Name Is</span>
-                </p>
+            <div className="home__wrapper">
+                <div className="home__container container">
+                    <p className="home__subtitle text-cs">
+                        Hello, <span>My Name Is</span>
+                    </p>
 
-                <h1 className="home__title text-cs">
-                    <span>ZOÉ</span> MILLER
-                </h1>
+                    <h1 className="home__title text-cs">
+                        <span>ZOÉ</span> MILLER
+                    </h1>
 
-                <p className="home__job">
-                    <span className="text-cs">I Am</span> <b>Web Developer</b>
-                </p>
+                    <p className="home__job">
+                        <span className="text-cs">I Am</span> <b>Web Developer</b>
+                    </p>
 
-                <div className="home__img-wrapper">
-                    <div className="home__banner">
-                        <img src={profileImg} alt="" className='home__profile' />
+                    <div className="home__img-wrapper">
+                        <div className="home__banner">
+                            <img src={profileImg} alt="" className='home__profile' />
+                        </div>
+
+                        <p className="home__data home__data-one">
+                            <span className="text-lg">
+                                12 <b>+</b>
+                            </span>
+
+                            <span className="text-sm text-cs">
+                                Years of <span>Experience</span>
+                            </span>
+                        </p>
+
+                        <p className="home__data home__data-two">
+                            <span className="text-lg">
+                                330
+                            </span>
+
+                            <span className="text-sm text-cs">
+                                Completed <span>Projects</span>
+                            </span>
+                        </p>
+
+                        <img src={shapeOne} alt="" className="shape shape__1" />
+                        <img src={shapeTwo} alt="" className="shape shape__2" />
+                        <img src={shapeTwo} alt="" className="shape shape__3" />
                     </div>
 
-                    <p className="home__data home__data-one">
-                        <span className="text-lg">
-                            12 <b>+</b>
-                        </span>
-
-                        <span className="text-sm text-cs">
-                            Years of <span>Experience</span>
-                        </span>
+                    <p className="home__text">
+                        From France, Paris. I have rich experience in web design, also I am
+                        good at wordpress. I love to talk with you about our unique
                     </p>
 
-                    <p className="home__data home__data-two">
-                        <span className="text-lg">
-                            330
-                        </span>
+                    <div className="home__socials">
+                        <a href="" className="home__social-link">
+                            <FaTwitter />
+                        </a>
 
-                        <span className="text-sm text-cs">
-                            Completed <span>Projects</span>
-                        </span>
-                    </p>
+                        <a href="" className="home__social-link">
+                            <FaDribbble />
+                        </a>
 
-                    <img src={shapeOne} alt="" className="shape shape__1" />
-                    <img src={shapeTwo} alt="" className="shape shape__2" />
-                    <img src={shapeTwo} alt="" className="shape shape__3" />
-                </div>
+                        <a href="" className="home__social-link">
+                            <FaBehance />
+                        </a>
+                    </div>
 
-                <p className="home__text">
-                    From France, Paris. I have rich experience in web design, also I am
-                    good at wordpress. I love to talk with you about our unique
-                </p>
+                    <div className="home__btns">
+                        <a href="" className="btn text-cs">
+                            Download CV
+                        </a>
 
-                <div className="home__socials">
-                    <a href="" className="home__social-link">
-                        <FaTwitter />
-                    </a>
-
-                    <a href="" className="home__social-link">
-                        <FaDribbble />
-                    </a>
-
-                    <a href="" className="home__social-link">
-                        <FaBehance />
-                    </a>
-                </div>
-
-                <div className="home__btns">
-                    <a href="" className="btn text-cs">
-                        Download CV
-                    </a>
-
-                    <a href="" className="hero__link text-cs">
-                        My Skills
-                    </a>
+                        <a href="" className="hero__link text-cs">
+                            My Skills
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
