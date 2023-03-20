0c0d5a3fb921c4e9c67080782fe00297ae213582
ErickDC <erickdiazc717@gmail.com>
2023-03-20 11:25:13 -0500

    skills component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './components/home/Home'
 import Services from './components/services/Services'
+import Skills from './components/skills/Skills'
 
 function App() {
   return (
     <main className="main">
       <Home />
       <Services />
+      <Skills />
     </main>
   )
 }
