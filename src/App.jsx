d3da0f99ba51fd72baba5b20d1d8dd85bb295363
ErickDC <erickdiazc717@gmail.com>
2023-03-20 13:22:52 -0500

    resume component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './components/home/Home'
 import Portfolio from './components/portfolio/Portfolio'
+import Resume from './components/resume/Resume'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
 
@@ -11,6 +12,7 @@ function App() {
       <Services />
       <Skills />
       <Portfolio />
+      <Resume />
     </main>
   )
 }
