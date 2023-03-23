8a093b314b23917510a8d9e034c1ea7a80ee988c
ErickDC <erickdiazc717@gmail.com>
2023-03-23 12:39:12 -0500

    bg title added

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -138,6 +138,10 @@ const Pricing = () => {
             <div className="section__deco deco__left">
                 <img src={shapeOne} alt="" className='shape' />
             </div>
+
+            <div className="section__bg-wrapper">
+                <span className="bg__title">Pricing</span>
+            </div>
         </section>
     )
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -61,6 +61,10 @@ const Resume = () => {
             <div className="section__deco deco__left">
                 <img src={shapeOne} alt="" className='shape' />
             </div>
+
+            <div className="section__bg-wrapper">
+                <span className="bg__title">History</span>
+            </div>
         </section>
     )
 }
